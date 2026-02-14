5ce8526e44159b504bda9be4f7ca4bf797908a5f
asapqlead <chhomalead@gmail.com>
2026-02-14 22:17:20 +0700

    Implement Blynk control with DHT11 and IR sensor

---
+++ lab3/task1.py
@@ -0,0 +1,82 @@
+import network
+import time
+import machine
+import dht
+import urequests as requests
+
+# ---------- CONFIG ----------
+WIFI_SSID = "AUPP Wifi"
+WIFI_PASS = ""
+
+BLYNK_TOKEN = "AXLHua5Akq9qdlm3L9z4dTcq0-QaA8MF"
+BLYNK_API   = "http://blynk.cloud/external/api"
+
+LED_PIN = 2
+DHT_PIN = 4
+
+# ---------- HARDWARE ----------
+led = machine.Pin(LED_PIN, machine.Pin.OUT)
+sensor = dht.DHT11(machine.Pin(DHT_PIN))
+
+# ---------- WIFI ----------
+wifi = network.WLAN(network.STA_IF)
+wifi.active(True)
+wifi.connect(WIFI_SSID, WIFI_PASS)
+
+print("Connecting to WiFi...")
+while not wifi.isconnected():
+    time.sleep(1)
+print("WiFi connected!")
+
+# ---------- BLYNK ----------
+def read_button_v0():
+    r = requests.get(f"{BLYNK_API}/get?token={BLYNK_TOKEN}&V0")
+    value = int(str(r.text).strip('[]"{}'))
+    r.close()
+    return value
+
+def send_temperature_v1(temp):
+    url = f"{BLYNK_API}/update?token={BLYNK_TOKEN}&V1={temp}"
+    r = requests.get(url)
+    r.close()
+
+def send_ir_signal(ir):
+     url = f"{BLYNK_API}/update?token={BLYNK_TOKEN}&V0={ir}"
+     r = requests.get(url)
+     r.close()
+
+def send_ir_signal_str(ir):
+     url = f"{BLYNK_API}/update?token={BLYNK_TOKEN}&V1={ir}"
+     r = requests.get(url)
+     r.close()
+  
+ir = machine.Pin(12, machine.Pin.IN)
+
+# ---------- MAIN ----------
+print("Running Blynk control...")
+
+while True:
+    # Button → LED
+    #if read_button_v0() == 1:
+     #   led.on()
+    #else:
+     #   led.off()
+    value = ir.value()
+    
+    if value == 0:
+        send_ir_signal(0)
+        #send_ir_signal("Obstacle detected",'1')
+        send_ir_signal_str("Detected")
+        print("Obstacle detected")
+    else:
+        send_ir_signal(1)
+        send_ir_signal_str("NotDetected")
+        #send_ir_signal("No obstacle", '1')
+        print("No obstacle")
+    # Temperature → Gauge
+    #sensor.measure()
+    #temp = sensor.temperature()
+    #print("Temperature:", temp)
+    #send_temperature_v1(temp)
+
+    time.sleep(1)
